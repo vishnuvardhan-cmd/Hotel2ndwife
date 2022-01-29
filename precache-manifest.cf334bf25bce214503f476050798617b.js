ce497203e0d2760b1ffcb655b8b2d39de5c1bf14
Vishnuvardhan Bandlamudi <bvishnu123vardhan@gmail.com>
2022-01-29 15:46:12 +0530

    Updates

---
+++ precache-manifest.cf334bf25bce214503f476050798617b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c525ae3f4ab6f31d56dbb884faaeeaa",
+    "url": "/Hotel2ndwife/index.html"
+  },
+  {
+    "revision": "b40b8c4bc75e8896ec2a",
+    "url": "/Hotel2ndwife/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/Hotel2ndwife/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hotel2ndwife/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40b8c4bc75e8896ec2a",
+    "url": "/Hotel2ndwife/static/js/main.11bebaa1.chunk.js"
+  },
+  {
+    "revision": "b3277210edac1bd7de6e",
+    "url": "/Hotel2ndwife/static/js/runtime-main.523cf478.js"
+  }
+]);
